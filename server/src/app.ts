b0e78e141fb24ee94db30b600db056628129ecd3
AndreyNedelevich <nedelevich@gmail.com>
2023-07-11 20:11:03 +0300

    Vercel

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,15 @@
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import fileUpload from "express-fileupload";
+import * as mongoose from "mongoose";
 
 import { configs } from "./configs/config";
+import { cronRunner } from "./crons";
 import { ApiError } from "./errors";
 import { authRouter } from "./routers/auth.router";
 import { userRouter } from "./routers/user.router";
 
+const PORT = configs.API_PORT || 5110;
 const app = express();
 
 app.use(express.json());
@@ -30,6 +33,12 @@ app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
     message: err.message,
     status: err.status,
   });
+});
+
+app.listen(PORT, async () => {
+  await mongoose.connect(configs.MONGODB_URL);
+  cronRunner();
+  console.log(`Server has started on PORT $${PORT} 🥸`);
 });
 
 export default app;
