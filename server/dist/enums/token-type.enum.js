95f7174c6e67b4a820f89c6e55e7cd9d1ca34ef3
AndreyNedelevich.github.io <113480738+AndreyNedelevich@users.noreply.github.com>
2024-02-04 11:47:06 +0200

    Delete server/dist directory
    Delete

--- server/dist/enums/token-type.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ETokenType = void 0;
-var ETokenType;
-(function (ETokenType) {
-    ETokenType["Refresh"] = "refresh";
-    ETokenType["Access"] = "access";
-})(ETokenType || (exports.ETokenType = ETokenType = {}));
